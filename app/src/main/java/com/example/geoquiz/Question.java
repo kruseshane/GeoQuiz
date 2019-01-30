3dd84835dd5078a2f4e395a83371c730a1fc6651
kruseshane <32747753+kruseshane@users.noreply.github.com>
2019-01-30 10:06:12 -0500

    Checkpoint 2

---
+++ app/src/main/java/com/example/geoquiz/Question.java
@@ -0,0 +1,21 @@
+package com.example.geoquiz;
+
+public class Question {
+
+    private int question;
+    private boolean answer;
+
+    public Question(int question, boolean answer) {
+        this.question = question;
+        this.answer = answer;
+    }
+
+    public int getQuestion() {
+        return question;
+    }
+
+    public boolean isAnswer() {
+        return answer;
+    }
+
+}
--- app/src/main/java/com/example/geoquiz/QuizActivity.java
+++ app/src/main/java/com/example/geoquiz/QuizActivity.java
@@ -2,25 +2,47 @@ package com.example.geoquiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import org.w3c.dom.Text;
 
 public class QuizActivity extends AppCompatActivity {
 
     private Button mTrueButton;
     private Button mFalseButton;
+    private Button mNextButton;
+    private TextView questionTV;
+    private int currentIndex = 0;
+
+    // "This is not a good way of doing it" - Dr. De
+    private Question [] qBank = new Question[] {
+            new Question(R.string.question_1, true),
+            new Question(R.string.question_2, false),
+            new Question(R.string.question_3, true),
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
 
+        questionTV = (TextView) findViewById(R.id.question_text);
+        int question = qBank[currentIndex].getQuestion();
+        questionTV.setText(question);
+
         mTrueButton = findViewById(R.id.true_button);
         mTrueButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(QuizActivity.this, R.string.correct_toast, Toast.LENGTH_SHORT).show();
+                if (qBank[currentIndex].isAnswer()) {
+                    Toast.makeText(QuizActivity.this, R.string.correct_toast, Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(QuizActivity.this, R.string.incorrect_toast, Toast.LENGTH_SHORT).show();
+                }
             }
         });
 
@@ -28,7 +50,22 @@ public class QuizActivity extends AppCompatActivity {
         mFalseButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(QuizActivity.this, R.string.incorrect_toast, Toast.LENGTH_SHORT).show();
+                if (!qBank[currentIndex].isAnswer()) {
+                    Toast.makeText(QuizActivity.this, R.string.correct_toast, Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(QuizActivity.this, R.string.incorrect_toast, Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        mNextButton = findViewById(R.id.next_button);
+        mNextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("TAG", String.valueOf(currentIndex));
+                currentIndex = (currentIndex + 1) % qBank.length;
+                int question = qBank[currentIndex].getQuestion();
+                questionTV.setText(question);
             }
         });
     }
